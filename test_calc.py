b3c2b585f70250bb9333b6433a961f10ce610e47
YousefElnahass <Y.nasr2211@nu.edu.eg>
2024-11-16 22:07:56 +0200

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -1,12 +1,13 @@
 import unittest
 from calc import add
-
+#mohamed yehia 221001815             youssef nasr            221000911
 class TestCalc(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(3, 3), 6)
-        self.assertEqual(add(-1, -3), -4)
+        self.assertEqual(add(3, 1), 4)
         self.assertEqual(add(0, 0), 0)
-        self.assertEqual(add(-1, 2), 1)
+        self.assertEqual(add(-5, -2), -7)
+        self.assertEqual(add(4, -2), 2)
+        
 
 if __name__ == '__main__':
     unittest.main()
